681b1fe4b857d17d34b5459932cea0e4c5e8a463
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-08-01 16:54:41 -0300

    Setup SSL

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,8 +1,6 @@
 import express from "express";
 import "reflect-metadata";
 import { createConnection } from "typeorm";
-import { handleError } from "./middlewares/handleError";
-import { router } from "./routes";
 import { env } from "./env";
 import cors from "cors";
 import https from "https";
@@ -24,9 +22,6 @@ import fs from "fs";
   app.get("/health", (req, res) => {
     res.send("Ok");
   });
-
-  app.use(router);
-  app.use(handleError);
 
   
   if(env.USE_HTTPS === "true") {
