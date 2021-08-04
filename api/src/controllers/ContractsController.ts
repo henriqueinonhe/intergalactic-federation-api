97cff3855be31261d63c7831191a4126cac8cb45
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-08-04 16:11:35 -0300

    Implement Contracts service and controller

---
+++ api/src/controllers/ContractsController.ts
@@ -0,0 +1,22 @@
+import { wrapAsyncController } from "../helpers/wrapController";
+import { ContractsService } from "../services/ContractsService";
+
+export class ContractsController {
+  public static createContract = wrapAsyncController(async (req, res, next) => {
+    const contractCreationData = req.body;
+
+    const createdContract = await ContractsService.createContract(contractCreationData);
+
+    res.status(201).send(createdContract);
+    next();
+  });
+
+  public static getContracts = wrapAsyncController(async (req, res, next) => {
+    const query = req.query;
+
+    const contracts = await ContractsService.getContracts(query);
+    
+    res.send(contracts);
+    next();
+  });
+}
