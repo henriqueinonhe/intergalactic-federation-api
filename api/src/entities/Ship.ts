30ffb25dd4c17132ee6b090e3a8fc3ed4595891c
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-08-04 16:02:19 -0300

    Implement Ship entity

---
+++ api/src/entities/Ship.ts
@@ -0,0 +1,57 @@
+import { Column, CreateDateColumn, Entity, OneToOne, PrimaryColumn, UpdateDateColumn } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { Pilot } from "./Pilot";
+
+@Entity("Ships")
+export class Ship {
+  @PrimaryColumn({
+    type: "varchar",
+    nullable: false
+  })
+  public id : string;
+
+  @Column({
+    type: "int",
+    nullable: false
+  })
+  public fuelCapacity : number;
+
+  @Column({
+    type: "int",
+    nullable: false
+  })
+  public fuelLevel : number;
+
+  @Column({
+    type: "int",
+    nullable: false
+  })
+  public weightCapacity : number;
+
+  @Column({
+    type: "int",
+    nullable: false
+  })
+  public currentWeight : number;
+
+  @OneToOne(() => Pilot, pilot => pilot.ship)
+  public pilot : Pilot | null;
+
+  @CreateDateColumn({
+    type: "timestamp",
+    nullable: false
+  })
+  public createdAt : Date;
+
+  @UpdateDateColumn({
+    type: "timestamp",
+    nullable: false
+  })
+  public updatedAt : Date;
+
+  constructor() {
+    if(!this.id) {
+      this.id = uuid();
+    }
+  }
+}
