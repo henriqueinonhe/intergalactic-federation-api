e3534027255c824aa44c41ed1976684965ce8f91
Henrique Inonhe <henriqueinonhe@gmail.com>
2021-08-04 16:17:11 -0300

    Implement routes

---
+++ api/src/routes.ts
@@ -0,0 +1,44 @@
+import express from "express";
+import { ContractsController } from "./controllers/ContractsController";
+import { PilotsController } from "./controllers/PilotsController";
+import { ReportsController } from "./controllers/ReportsController";
+import { ResourcesController } from "./controllers/ResourcesController";
+import { ShipsController } from "./controllers/ShipsController";
+
+export const router = express.Router();
+
+//Pilots
+router.route("/pilots")
+  .post(PilotsController.createPilot);
+
+router.route("/pilots/:id/travel")
+  .put(PilotsController.travel);
+
+router.route("/pilots/:id/refuel")
+  .put(PilotsController.refuel);
+
+router.route("/pilots/:id/acceptContract")
+  .put(PilotsController.acceptContract);
+
+//Ships
+router.route("/ships")
+  .post(ShipsController.createShip);
+
+//Contracts
+router.route("/contracts")
+  .post(ContractsController.createContract)
+  .get(ContractsController.getContracts);
+
+//Reports
+router.route("/reports/planetsResourcesSummary")
+  .get(ReportsController.getPlanetsResourcesSummary);
+
+router.route("/reports/pilotsResourcesSummary")
+  .get(ReportsController.getPilotsResourcesSummary);
+
+router.route("/reports/transactionsLedger")
+  .get(ReportsController.getTransactionsLedger);
+
+//Resources
+router.route("/resources")
+  .post(ResourcesController.createResource);
